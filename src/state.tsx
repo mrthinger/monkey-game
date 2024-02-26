2666e320b1de697f1db9ceadd375ef9953c76f93
Evan Pierce <evan@mrthinger.com>
2024-02-25 17:07:41 -0800

    ECS v1

--- src/state.tsx
+++ src/state.tsx
@@ -2,28 +2,30 @@ import typesafeContextHook from "typesafe-context-hook";
 import { useList } from "react-use";
 import { World } from "miniplex";
 import { useState } from "react";
-import createReactAPI from "miniplex-react"
+import createReactAPI from "miniplex-react";
 import { Vector3 } from "@react-three/fiber";
-
+import { Sprite } from "three";
 
 // type enum for banana, monkey, farm
+enum EntityType {
+  Banana,
+  Monkey,
+  Farm,
+  Trap,
+}
+
+type Health = {
+  current: number;
+  max: number;
+};
+
 type Entity = {
+  type: EntityType.Monkey;
   position: Vector3;
-  isBanana: boolean;
+  velocity: Vector3;
+  sprite: Sprite;
 };
 
 const world = new World<Entity>();
 
-export const ECS = createReactAPI(world)
-
-// export const { useGameState, GameStateProvider } = typesafeContextHook(
-//   "GameState",
-//   () => {
-
-//     world.add({ position: new Vector3(), isBanana: true });
-
-//     const bananasQuery = world.with("isBanana");
-
-//     return { bananasQuery, world };
-//   }
-// );
+export const ECS = createReactAPI(world);
